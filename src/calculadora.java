5022e0e720e9d06ed7aaa06b818c62f0827b7308
Aitor Santana <Aitor Santana>
2016-04-08 17:32:13 +0100

    pasos de documentación en la rama master

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,8 +1,24 @@
+ /**
 
+ * Esta clase se usará para hacer un examen de ETS
+ * 
+ * @author: Aitor Arnaldo Santana Ortega
+ * @version: 8/4/2016
+ * @see <a href = "http://www.aitorsantana.com" /> aitorsantana.com – Mi web personal </a>
+ */
 public class calculadora {
 
-    private String op1;
+    /* @param En realidad se debería llamar 'operador1' pero lo hice corriendo y la fastidié.
+     *  Sirve para usarlo de operando.*/
+	
+	private String op1;
+	
+    /* @param En realidad se debería llamar 'operador2' pero lo hice corriendo y la fastidié.
+     *  Sirve para usarlo de operador.*/
     private String op2;
+    
+    /* @param En realidad se debería llamar 'operación' pero lo hice corriendo y la fastidié.
+     *  Sirve para usarlo de operación.*/
     private String res;
     
     public static void main(String[] args) {
