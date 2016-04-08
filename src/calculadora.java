aea8d47b8aca6fffea37c23b9829276d564b3867
Aitor Santana <Aitor Santana>
2016-04-08 17:03:57 +0100

    paso 1 y 2

---
+++ src/calculadora.java
@@ -0,0 +1,15 @@
+
+public class calculadora {
+
+    private String op1;
+    private String op2;
+    private String res;
+    
+    public static void main(String[] args) {
+    	///codigo
+    }
+
+
+
+    
+}
